edf7fe2f326c9c64812348fbc1d2439b3c9d9ef9
이바랑 <nb040705@gmail.com>
2024-06-21 00:10:33 +0900

    initail commit

--- link.cpp
+++ link.cpp
@@ -2,6 +2,7 @@
 #include "node.h"
 #include <iostream>
 
+// 패킷을 반대편으로 보낸다.
 void Link::sendPacket(Node* sender, Packet* packet){
     std::string message1 = "packet in: " + packet->toString() + " from " + sender->toString();
     log(message1);
